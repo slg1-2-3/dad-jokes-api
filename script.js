89f7cc32ce486edec721a7d3cdbcab9fe49ff1c5
simone <simonegraham42@gmail.com>
2025-10-11 13:43:47 -0400

    added function to get joke

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ function setBackgroundColor(id) {
     element.style.backgroundColor = "rgb(0,0,0)"
 }
 
+async function getJoke(){
+    const url = "https://icanhazdadjoke.com/"
+    try {
+        const response = await fetch(url, {headers: {"Accept": "application/json"}})
+        if (!response.ok) {
+            throw new Error(`Response Status: ${response.status}`);
+        }
+   
+    const result = await response.json();
+    console.log(result, result.joke);    
+    }
+    catch (error) {console.error(error.message)} 
+};
+
 const btn = document.querySelector("#jokeBtn");
 
 btn.addEventListener("click", () => {
     setBackgroundColor("#jokeBtn");
+    getJoke();
 })
